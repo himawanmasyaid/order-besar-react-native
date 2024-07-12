2fc81de7b2f56979961ec9ff73399b76dc743f55
himawan.masyaid <himawan.masyaid@gmail.com>
2024-07-12 23:11:54 +0700

    create : install and setup library

--- App.tsx
+++ App.tsx
@@ -1,10 +1,26 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import textstyle from 'styles/textstyle';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    poppins_bold: require("./assets/fonts/poppins_bold.ttf"),
+    poppins_regular: require("./assets/fonts/poppins_regular.ttf"),
+    poppins_semibold: require("./assets/fonts/poppins_semibold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      // setIsLoading(false);
+    }
+  }, [fontsLoaded]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={textstyle.TextParagraph1Bold}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
