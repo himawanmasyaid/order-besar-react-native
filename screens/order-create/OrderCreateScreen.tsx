d03dd703c47148bb4209cefc33b771553e59c61d
himawan.masyaid <himawan.masyaid@gmail.com>
2024-07-13 11:07:37 +0700

    create : confirm delete order modal dialog

--- screens/order-create/OrderCreateScreen.tsx
+++ screens/order-create/OrderCreateScreen.tsx
@@ -1,4 +1,6 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { ProductRepository } from "data/repository/ProductRepository";
+import { GetProductUseCase } from "data/usecases/GetProductUseCase";
 import { RootStackParamList } from "navigation/RootStackParamList";
 import { SafeAreaView, StyleSheet } from "react-native";
 import containerstyle from "styles/containerstyle";
@@ -6,15 +8,14 @@ import containerstyle from "styles/containerstyle";
 const OrderCreateScreen = ({
   navigation,
 }: NativeStackScreenProps<RootStackParamList>) => {
-  return (
-    <SafeAreaView style={containerstyle.safearea}>
-        
-    </SafeAreaView>
-  );
+  const productRepository = new ProductRepository(new GetProductUseCase());
+
+    // const product = await productRepository.getProduct();
+    // console.log("product response :", product);
+
+  return <SafeAreaView style={containerstyle.safearea}></SafeAreaView>;
 };
 
-const styles = StyleSheet.create({
-
-})
+const styles = StyleSheet.create({});
 
 export default OrderCreateScreen;
--- screens/order/OrderScreen.tsx
+++ screens/order/OrderScreen.tsx
@@ -7,8 +7,9 @@ import { GetOrderUseCase } from "data/usecases/GetOrderUseCase";
 import { GetProductUseCase } from "data/usecases/GetProductUseCase";
 import { RootStackParamList } from "navigation/RootStackParamList";
 import { useEffect, useState } from "react";
-import { FlatList, SafeAreaView, StyleSheet, View } from "react-native";
+import { FlatList, Modal, SafeAreaView, StyleSheet, Text, View, TouchableOpacity} from "react-native";
 import containerstyle from "styles/containerstyle";
+import textstyle from "styles/textstyle";
 
 const OrderScreen = () => {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
@@ -19,6 +20,7 @@ const OrderScreen = () => {
   const [page, setPage] = useState(1);
   const [limit, setLimit] = useState(10);
   const [isHasMore, setHasMore] = useState(true);
+  const [isShowDeleteModal, setShowDeleteModal] = useState(false);
 
   const fetchOrders = async () => {
     console.log("fetchOrders");
@@ -50,11 +52,75 @@ const OrderScreen = () => {
             order={item}
             onEditPress={() => navigation.navigate('order_edit', item) }
             onDetailPress={() => navigation.navigate('order_detail', item)}
-            onDeletePress={() => console.log("delete press") }
+            onDeletePress={() => setShowDeleteModal(true) }
           />
         )}
         keyExtractor={(item) => item.id}
       />
+
+      {/* modal popup delete order */}
+
+      <View style={{ flex: 1 }}>
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible={isShowDeleteModal}
+          onRequestClose={() => setShowDeleteModal(false)}
+        >
+          <View style={styles.modal_overlay}>
+            <View
+              style={{
+                backgroundColor: "#fff",
+                borderRadius: 8,
+                padding: 16,
+                width: "90%",
+                justifyContent: "center",
+                alignItems: "center",
+                marginHorizontal: 16,
+              }}
+            >
+              <Text style={textstyle.TextParagraph1Bold}>
+                Are You Sure To Delete This?
+              </Text>
+
+              <Text
+                style={[
+                  textstyle.TextParagraph2,
+                  { textAlign: "center", marginTop: 20 },
+                ]}
+              >
+                You can't recover data because it will be deleted permanently.
+              </Text>
+
+              <TouchableOpacity
+                  style={
+                    styles.button_outline_alert_modal
+                  }
+                >
+                  <Text
+                    style={[textstyle.TextParagraph1Bold, { color: "#EB5757" }]}
+                  >
+                    Yes, Delete It
+                  </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  onPress={() => setShowDeleteModal(false)}
+                  style={
+                    styles.button_primary
+                  }
+                >
+                  <Text
+                    style={[textstyle.TextParagraph1Bold, { color: "#fff" }]}
+                  >
+                    Back
+                  </Text>
+                </TouchableOpacity>
+
+            </View>
+          </View>
+        </Modal>
+      </View>
 
       </View>
     </SafeAreaView>
@@ -66,6 +132,58 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
   },
+
+  // modal popup style
+
+  modal_button_container: {
+    marginTop: 20,
+    flex: 1,
+  },
+
+  modal_container: {
+    flexDirection: "column",
+    width: "90%",
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    padding: 20,
+  },
+
+  modal_overlay: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)", // add a dark overlay to dim the background
+  },
+
+  button_outline_alert_modal: {
+    justifyContent: "center", 
+    alignItems: "center",
+    borderColor: "#EB5757",
+    borderWidth: 1,
+    backgroundColor: "#fff",
+    marginBottom: 16,
+    paddingVertical: 8,
+    marginTop: 24,
+    width: "100%",
+    borderRadius: 4
+  },
+
+  button_primary: {
+    justifyContent: "center", 
+    alignItems: "center",
+    borderWidth: 1,
+    backgroundColor: "#052A49",
+    paddingVertical: 8,
+    width: "100%",
+    borderRadius: 4
+  },
+
+  wrapper_center: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+
 })
 
 export default OrderScreen;
